b887283965405e2be06e8bbe5ab1625571485682
Anton Vanin <gbiostrain@gmail.com>
2022-11-27 15:50:24 +0300

    02-javascript-data-types/3-omit

--- 02-javascript-data-types/3-omit/index.js
+++ 02-javascript-data-types/3-omit/index.js
@@ -5,5 +5,12 @@
  * @returns {object} - returns the new object
  */
 export const omit = (obj, ...fields) => {
-
+  return Object
+    .entries(obj)
+    .reduce((picked, [key, value]) => {
+      if (!fields.includes(key)) {
+        picked[key] = value;
+      }
+      return picked;
+    }, {});
 };
