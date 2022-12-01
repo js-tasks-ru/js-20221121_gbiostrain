e9f7e4d22614179909d8c52a30c04c8f8d9ea46a
Anton Vanin <gbiostrain@gmail.com>
2022-12-01 07:47:24 +0300

    03-objects-arrays-intro-to-testing/1-create-getter

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,6 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
-
+  return (source) =>
+    path.split(".").reduce((value, field) => value?.[field], source);
 }
