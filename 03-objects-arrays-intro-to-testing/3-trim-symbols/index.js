e88229c36526cd780bcf7d645b7dc93953782880
Anton Vanin <gbiostrain@gmail.com>
2022-12-01 08:20:38 +0300

    03-objects-arrays-intro-to-testing/3-trim-symbols

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,22 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  if (!Number.isInteger(size)) {
+    return string;
+  }
 
+  let counter = 0;
+
+  return string
+    .split('')
+    .reduce((acc, element, index) => {
+      counter = element === string[index - 1] ? counter + 1 : 1;
+
+      if (counter <= size) {
+        acc.push(element);
+      }
+
+      return acc;
+    }, [])
+    .join('');
 }
