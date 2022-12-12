1183a5adeb96c799bfee27bf892ebc7ca64ffce4
Anton Vanin <gbiostrain@gmail.com>
2022-12-12 08:23:20 +0300

    06-events-practice/2-tooltip

--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -1,6 +1,60 @@
 class Tooltip {
-  initialize () {
+  static #instance;
+  #element;
 
+  constructor() {
+    if (!Tooltip.#instance) {
+      Tooltip.#instance = this;
+    }
+
+    return Tooltip.#instance;
+  }
+
+  initialize() {
+    document.addEventListener("pointerover", this.onPointerOver);
+  }
+
+  get element() {
+    if (!this.#element) {
+      this.#element = document.createElement('div');
+      this.#element.className = 'tooltip';
+    }
+
+    return this.#element;
+  }
+
+  render(text = '') {
+    this.element.innerHTML = text;
+    document.body.append(this.element);
+  }
+
+  onPointerOver = (event) => {
+    const target = event.target.closest('[data-tooltip]');
+    if (!target) return;
+
+    this.render(target.dataset.tooltip);
+
+    target.addEventListener("pointerout", this.onPointerOut, { once: true });
+    document.addEventListener("pointermove", this.onPointerMove);
+  }
+
+  onPointerMove = (event) => {
+    this.element.style.top = `${event.pageY + 5}px`;
+    this.element.style.left = `${event.pageX + 5}px`;
+  }
+
+  onPointerOut = () => {
+    this.remove();
+  }
+
+  remove() {
+    document.removeEventListener("pointermove", this.onPointerMove);
+    this.#element.remove();
+  }
+
+  destroy() {
+    document.removeEventListener("pointerover", this.onPointerOver);
+    this.remove();
   }
 }
 
